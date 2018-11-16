752343965d522800778ae93ba1aafe5d1d57c4b4
Chris Herrera <chris@Chriss-MacBook-Pro.local>
2018-11-16 08:29:25 -0600

    adding a second well

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -103,3 +103,41 @@ values(10001,'Hashmap','-6:00','Streaming','Connected','c6d249d2-12bc-47a3-91fd-
    <GroundElevation uom="ft" datum="unknown">250</GroundElevation>
 </well>
 ');
+
+insert into well (id,company,time_zone,streaming_state,live_state,uuid,name,data)
+values(10002,'Hashmap','+1:00','Streaming','Connected','e03e86c7-72c1-414e-aecc-8ea3c4','EnergisticsWell2016-E','<Well xmlns="http://www.energistics.org/energyml/data/witsmlv2"
+      xmlns:eml="http://www.energistics.org/energyml/data/commonv2"
+      xmlns:gco="http://www.isotc211.org/2005/gco"
+      xmlns:gmd="http://www.isotc211.org/2005/gmd"
+      xmlns:gsr="http://www.isotc211.org/2005/gsr"
+      xmlns:gts="http://www.isotc211.org/2005/gts"
+      xmlns:gml="http://www.opengis.net/gml/3.2"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.energistics.org/energyml/data/witsmlv2 file:///C:/energyml/data/witsml/v2.0/xsd_schemas/Well.xsd"
+      schemaVersion="2.0"
+      uuid="e03e86c7-72c1-414e-aecc-8ea3c4">
+   <eml:Aliases authority="1.4.1.1">
+      <eml:Identifier>e03e86c7-72c1-414e-aecc-8ea3c4</eml:Identifier>
+      <eml:Description>Identifier is created from the uid of the input scheme</eml:Description>
+   </eml:Aliases>
+   <eml:Citation>
+      <eml:Title>EnergisticsWell2016-E</eml:Title>
+      <eml:Originator>1411_input</eml:Originator>
+      <eml:Creation>2018-11-16T14:21:24.937Z</eml:Creation>
+      <eml:Format>Mapforce</eml:Format>
+      <eml:LastUpdate>2016-04-05T11:33:43.755Z</eml:LastUpdate>
+   </eml:Citation>
+   <TimeZone>+01:00</TimeZone>
+   <StatusWell>drilling</StatusWell>
+   <PurposeWell>unknown</PurposeWell>
+   <WellDatum uid="SL">
+      <Name>Mean Sea Level</Name>
+      <Code>mean sea level</Code>
+      <Crs xsi:type="eml:VerticalUnknownCrs">
+         <eml:Unknown>unknown</eml:Unknown>
+      </Crs>
+   </WellDatum>
+</Well>
+');
+
